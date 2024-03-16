1350dc82cf34d7503e6fd5163cb1fe00e5b36ca2
Waqar-Akhtar <akhtarwaqar384@gmail.com>
2024-03-17 01:55:01 +0500

    port is updated and make it dynamic

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello Ec2  Instance");
 });
-app.listen(80, () => {
-  console.log("Server is running on port 3000");
+const PORT = 80;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });

337f5c1f41c334aca86324bc708f3f55a242eac8
Waqar-Akhtar <akhtarwaqar384@gmail.com>
2024-03-17 01:52:52 +0500

    update port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello Ec2  Instance");
 });
-app.listen(3000, () => {
+app.listen(80, () => {
   console.log("Server is running on port 3000");
 });

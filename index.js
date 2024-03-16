fe6a2d15890fb6f1685154a7f8fd755a7c7d1ff5
Waqar-Akhtar <akhtarwaqar384@gmail.com>
2024-03-17 02:08:30 +0500

    update port again and add /hello get api

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
+  res.send("Welcome Ec2  Instance");
+});
+app.get("/hello", (req, res) => {
   res.send("Hello Ec2  Instance");
 });
-const PORT = 80;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
